3ec7df3df101fca5d1fca60fe7e9e6b0c8c3608f
Irfan Arif <irfanarif444@gmail.com>
2024-08-01 01:06:32 +0500

    url fixedd

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"], // Ensure this matches your client-side origin
+    origin: ["https://bitchat-app.vercel.app/"], // Ensure this matches your client-side origin
     methods: ["GET", "POST"],
   },
 });
