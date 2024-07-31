4acf86cb00a4e05d5c88f15853e39b642b1cfd85
Irfan Arif <irfanarif444@gmail.com>
2024-08-01 01:04:58 +0500

    url fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://bitchat-app.vercel.app",
         // changeOrigin: true,
         changeOrigin: true,
         secure: false,
