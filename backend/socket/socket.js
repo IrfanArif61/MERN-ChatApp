2f9430618219123b888fd24d5243bed48f4d6b20
Irfan Arif <irfanarif444@gmail.com>
2024-08-01 01:47:10 +0500

    added link

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://bitchatapp.vercel.app/"],
+    origin: ["http://localhost:3000"], // Ensure this matches your client-side origin
     methods: ["GET", "POST"],
   },
 });
